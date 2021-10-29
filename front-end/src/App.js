90fc940e94334bccafddb62cb8ad6bbe25df5473
Bbek-Crestha <bcrestha07@gmail.com>
2021-10-29 14:43:57 +0545

    Add React-Bootstrap, Create navbar and footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,20 @@
+import { Container } from "react-bootstrap";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
 const App = () => {
 	return (
 		<div className="App">
-			<h1>Welcome to Best Shop.</h1>
+			<Header />
+
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to Best Shop.</h1>
+				</Container>
+			</main>
+
+			<Footer />
 		</div>
 	);
 };
