cb493fa58cb4612368c033d251e923f018332ab5
Bbek-Crestha <bcrestha07@gmail.com>
2021-11-05 07:49:28 +0545

    user registration and password encryption

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -2,10 +2,16 @@ import express from "express";
 const router = express.Router();
 import { protect } from "../middleware/authMiddleware.js";
 
-import { authUser, getUserProfile } from "../controller/userController.js";
+import {
+	authUser,
+	registerUser,
+	getUserProfile,
+} from "../controller/userController.js";
 
 router.post("/login", authUser);
 
 router.route("/profile").get(protect, getUserProfile);
+
+router.post("/", registerUser);
 
 export default router;
